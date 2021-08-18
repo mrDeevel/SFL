577a23093dd61b4ca066d5576f48e1734aa443e2
mrDeevel <67458950+mrDeevel@users.noreply.github.com>
2021-08-18 19:23:19 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
  /*   STEP 1   */
 export const leagueID = "717032410939744256"; // your league ID
-export const leagueName = "SFL League I"; // your league name
+export const leagueName = "Sleeper Fantasy Legion"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 
